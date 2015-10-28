e7760246d35d6f0eba95cd2a4139befa00b36c61
likedan <likedan5@icloud.com>
2015-10-27 23:41:18 -0500

    update

---
+++ README.md
@@ -0,0 +1,5 @@
+# View
+
+<img src="showup2.gif" width="320" height="550"/>
+
+
--- database.py
+++ database.py
@@ -10,28 +10,20 @@ class Database:
         client = MongoClient('127.0.0.1', 27017)
         print ("Connected successfully!!!")
         self.db = client['Policy']
-        # self.db["StatesTable"].remove()
-        self.policy = self.db['LearningTable']
+        # self.db["TrajectoryTable"].remove()
+        self.policy = self.db['TrajectoryTable']
 
-    def addNewStates(self, state):
-        stateObject = {"radius": state.radius, "speed": state.speedDif, "altitude": state.altitudeDif, "heading": state.headingDif, "trajectory": state.trajectoryDif, "award": state.calculateAward()}
-        state_id = self.policy.insert_one(stateObject).inserted_id
-        return state_id
+    def addNewRadius(self, radius):
+        stateObject = {"radius": radius}
+        self.policy.insert_one(stateObject)
 
+    def removeRadius(self, radius):
+        self.policy.remove({"radius": radius})
     # return None if state don't exist
-    def getState(self, state):
-        state = self.policy.find_one({"radius": state.radius, "speed": state.speedDif, "altitude": state.altitudeDif, "heading": state.headingDif, "trajectory": state.trajectoryDif})
+    def getRadius(self, radius):
+        state = self.policy.find_one({"radius": radius})
         return state
 
-    def getStateDict(self, id):
-        if type(id) == str:
-            id = ObjectId(id)
-        return self.policy.find_one({"_id": id})
-
-    def saveState(self, state):
-        logText = str(state["_id"]) + "  " + str(len(state["actions"].keys())) + "\n"
-        text_file = open("log.txt", "a")
-        text_file.write(logText)
-        text_file.close()
-        self.policy.update({'_id': state["_id"]}, state)
+    def saveRadius(self, radiusState):
+        self.policy.update({'_id': radiusState["_id"]}, radiusState)
     # def saveAction():
