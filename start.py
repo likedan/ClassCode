f4d8cd27f1b959ebfc796589c1d263f2009683cd
likedan <likedan5@icloud.com>
2015-12-13 22:21:51 -0600

    never dye

--- start.py
+++ start.py
@@ -1,8 +1,8 @@
 #!/Library/Frameworks/Python.framework/Versions/3.5/bin/python3
 import clgrd4a
-import Loop
+import LOOP
 # a = clgrd4a.clgrd4a()
 # print(a.PLAN(8000, 50))
 # print(a.PC(8000, 50))
-a = Loop.Pilot()
+a = LOOP.LOOP()
 a.start()
